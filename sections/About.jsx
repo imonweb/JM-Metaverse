393523caf5e756bb2495fd95a8e5fe1bce0b3d5a
Imon <contact@imonweb.co.uk>
2022-12-08 23:02:47 +0000

    about

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,7 +1,14 @@
 'use client';
 
+import  { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
     About section
   </section>
 );
